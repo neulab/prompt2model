50783c86ec9e5d001dbaa0e82484a632ebefa1c0
viswavi <vijayv@andrew.cmu.edu>
2023-04-05 11:00:34 -0400

    Fix integration test

--- tests/test_main.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-from prompt2model import main
-
-def test_integration():
-    prompt = ["Test prompt"]
-    main(prompt)
