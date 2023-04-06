45231dd581a9d579e82177c9628f6654f6ac5c78
zhaochen20 <zhaochenyang20@gmail.com>
2023-04-06 10:37:46 +0800

    add demo code for data generator

--- prompt2model/dataset_generator.py
+++ prompt2model/dataset_generator.py
@@ -1,8 +1,32 @@
-"""An interface for dataset generators.
+"""An interface for dataset generation.
 
 Input: A system description (optionally with few-shot examples)
 Output:
-   1) Path to local Jsonl file with training examples
-   2) Path to local Jsonl file with validation examples
-   3) Path to local Jsonl file with testing examples
+   1) training dataset
+   2) validation dataset
+   3) testing dataset
 """
+
+import datasets
+import pandas as pd
+from typing import Tuple
+from prompt_parser import PromptSpec
+
+
+class DatasetGenerator:
+    """A class for generating datasets from a prompt specification."""
+
+    def __init__(self):
+        """Construct a dataset generator."""
+        pass
+
+    def generate_datasets(
+        prompt_spec: PromptSpec,
+    ) -> Tuple[datasets.Dataset, datasets.Dataset, datasets.Dataset]:
+        """Generate training/validation/testing datasets from a prompt (which may
+        include a few demonstration examples). The typical implementation of this
+        will use a Large Language Model to generate a large number of examples."""
+        training = datasets.Dataset.from_pandas(pd.DataFrame({}))
+        validation = datasets.Dataset.from_pandas(pd.DataFrame({}))
+        testing = datasets.Dataset.from_pandas(pd.DataFrame({}))
+        return (training, validation, testing)
