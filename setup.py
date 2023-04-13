a735cbd689dae76221d9f75a24c9220b7f8ddc44
viswavi <vijayv@andrew.cmu.edu>
2023-04-13 13:11:50 -0400

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-from setuptools import setup, find_packages
+"""Install dependencies and build with pip."""
+from setuptools import find_packages, setup
 
 setup(
-    name='prompt2model',
-    version='0.0.1',
-    packages=find_packages(include=['prompt2model']),
+    name="prompt2model",
+    version="0.0.1",
+    packages=find_packages(include=["prompt2model"]),
     install_requires=[
-        'transformers==4.24.0',
-        'datasets==2.11.0'
-        'pandas==1.5.3',
-        'gradio==3.24.1'
-    ]
+        "transformers==4.24.0",
+        "datasets==2.11.0",
+        "pandas==1.5.3",
+        "gradio==3.24.1",
+    ],
 )
