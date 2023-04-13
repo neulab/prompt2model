1e5cef49efc470b94a5f339d60f858dd0b75c6b0
viswavi <vijayv@andrew.cmu.edu>
2023-04-13 13:09:44 -0400

    Add setup.py to contain dependencies

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='prompt2model',
+    version='0.0.1',
+    packages=find_packages(include=['prompt2model']),
+    install_requires=[
+        'transformers==4.24.0',
+        'datasets==2.11.0'
+        'pandas==1.5.3',
+        'gradio==3.24.1'
+    ]
+)
