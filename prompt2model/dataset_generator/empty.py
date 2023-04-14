8a15487094ce87aa6ef12469a77c2b3c63932444
viswavi <vijayv@andrew.cmu.edu>
2023-04-14 12:52:55 -0400

    Move empty dataset generator to empty.py

---
+++ prompt2model/dataset_generator/empty.py
@@ -0,0 +1,30 @@
+import datasets
+from dataset_generator.base import DatasetGenerator, DatasetSplit
+from prompt_parser import PromptSpec
+import pandas as pd
+
+class EmptyDatasetGenerator(DatasetGenerator):
+    """A class for generating empty datasets (for testing purposes)."""
+
+    def generate_examples(
+        self,
+        prompt_spec: PromptSpec,
+        num_examples: int,
+        split: DatasetSplit,
+    ) -> datasets.Dataset:
+        """Create empty versions of the datasets, for testing.
+
+        Args:
+            prompt_spec: A prompt specification.
+            num_examples: Number of examples in split.
+            split: Name of dataset split to generate.
+
+        Returns:
+            A single dataset split.
+
+        """
+        _ = prompt_spec, split  # suppress unused variable warnings
+        col_values = ["" for i in range(num_examples)]
+        # Construct empty-valued dataframe with length matching num_examples.
+        df = pd.DataFrame.from_dict({"test_col": col_values})
+        return datasets.Dataset.from_pandas(df)
