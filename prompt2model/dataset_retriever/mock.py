ead11b74d1c35a4ee50dabcd2c3790b850c38e26
viswavi <vijayv@andrew.cmu.edu>
2023-04-17 13:33:09 -0400

    Add mock class for DatasetRetriever

---
+++ prompt2model/dataset_retriever/mock.py
@@ -0,0 +1,20 @@
+"""A mock dataset retriever for testing purposes."""
+
+import datasets
+import pandas as pd
+
+from prompt2model.dataset_retriever.base import DatasetRetriever
+from prompt2model.prompt_parser import PromptSpec
+
+
+class MockRetriever(DatasetRetriever):
+    """A class for retrieving datasets."""
+
+    def __init__(self):
+        """Construct a mock dataset retriever."""
+
+    def retrieve_datasets(self, prompt_spec: PromptSpec) -> list[datasets.Dataset]:
+        """Return a single empty dataset for testing purposes."""
+        _ = prompt_spec  # suppress unused variable warning
+        test_df = pd.DataFrame.from_dict({"test_col": [""]})
+        return [datasets.Dataset.from_pandas(test_df)]
