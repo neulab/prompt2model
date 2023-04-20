31a79cc9872dca51ae379507a78d9f04bd110ff7
zhaochen20 <zhaochenyang20@gmail.com>
2023-04-20 16:57:55 +0800

    detach base trainer for DatasetGenrator

--- tests/dataset_generator_test.py
+++ tests/dataset_generator_test.py
@@ -1,4 +1,5 @@
 """Testing DatasetGenerator through DatasetGenerator."""
+
 import os
 import tempfile
 
@@ -8,45 +9,18 @@ from prompt2model.dataset_generator.base import DatasetSplit
 from prompt2model.dataset_generator.mock import MockDatasetGenerator
 
 
-def test_generate_examples():
-    """Generates examples using the MockDatasetGenerator.
-
-    Returns:
-        None
-    """
-    prompt_spec = None
-    num_examples = 10
-    split = DatasetSplit.TRAIN
-
-    dataset_generator = MockDatasetGenerator()
-    dataset = dataset_generator.generate_examples(prompt_spec, num_examples, split)
-
-    # Check that the generated dataset has the expected number of examples.
-    assert len(dataset) == num_examples
-
-    # Check that the generated dataset has the expected columns.
-    assert set(dataset.column_names) == {"input_col", "output_col"}
-
-    # Check that each example has the expected empty values.
-    expected_values = pd.Series("", index=dataset.column_names)
-    for example in dataset:
-        assert example == expected_values.to_dict()
-
-
 def test_generate_datasets():
     """Test the `generate_datasets()` function of a `MockDatasetGenerator` object.
 
-    The function generates datasets with the specified number
-    of examples for each split of the data (train, validation, test)
-    using a prompt specification and saves them to a temporary
-    directory. Then, it checks that the dataset dict has the expected
-    keys, that each split has the expected number of examples, that
-    each dataset has the expected columns, that each example has the
-    expected empty values, and that the dataset dict is saved to the
-    output directory.
-
-    Returns:
-        None
+    This function generates datasets by creating a specified
+    number of examples foreach split of the data, which includes
+    train, validation, and test. It uses "None"as the prompt
+    specification and saves the generated datasets to a temporary
+    directory. Afterward, the function checks whether the dataset
+    dictionary contains all the expected keys, each split has the
+    anticipated number of examples, every dataset has the anticipated
+    columns, each example has the expected empty values, and
+    whether the dataset dictionary is saved to the output directory.
     """
     prompt_spec = None
     num_examples = {DatasetSplit.TRAIN: 10, DatasetSplit.VAL: 5, DatasetSplit.TEST: 3}
@@ -84,3 +58,31 @@ def test_generate_datasets():
             "train",
             "val",
         }
+
+
+def test_generate_examples():
+    """Test the `generate_examples()` function of a `MockDatasetGenerator` object.
+
+    This function generates examples for a specified split of the data
+    (train, validation, or test) using "None" as the prompt specification
+    and saves them to a temporary directory. Then, it checks that the
+    generated dataset has the expected number of examples, the expected
+    columns, and each example has the expected empty values.
+    """
+    prompt_spec = None
+    num_examples = 10
+    split = DatasetSplit.TRAIN
+
+    dataset_generator = MockDatasetGenerator()
+    dataset = dataset_generator.generate_examples(prompt_spec, num_examples, split)
+
+    # Check that the generated dataset has the expected number of examples.
+    assert len(dataset) == num_examples
+
+    # Check that the generated dataset has the expected columns.
+    assert set(dataset.column_names) == {"input_col", "output_col"}
+
+    # Check that each example has the expected empty values.
+    expected_values = pd.Series("", index=dataset.column_names)
+    for example in dataset:
+        assert example == expected_values.to_dict()
