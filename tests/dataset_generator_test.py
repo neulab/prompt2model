d0d320a0d9ba65da14f542de4907a0c30834c537
zhaochen20 <zhaochenyang20@gmail.com>
2023-04-20 10:44:16 +0800

    fix lint

--- tests/dataset_generator_test.py
+++ tests/dataset_generator_test.py
@@ -1,11 +1,19 @@
-import pandas as pd
+"""Testing DatasetGenerator through DatasetGenerator."""
 import os
 import tempfile
-from prompt2model.dataset_generator.mock import MockDatasetGenerator
+
+import pandas as pd
+
 from prompt2model.dataset_generator.base import DatasetSplit
+from prompt2model.dataset_generator.mock import MockDatasetGenerator
 
 
 def test_generate_examples():
+    """Generates examples using the MockDatasetGenerator.
+
+    Returns:
+        None
+    """
     prompt_spec = None
     num_examples = 10
     split = DatasetSplit.TRAIN
@@ -26,13 +34,29 @@ def test_generate_examples():
 
 
 def test_generate_datasets():
+    """Test the `generate_datasets()` function of a `MockDatasetGenerator` object.
+
+    The function generates datasets with the specified number
+    of examples for each split of the data (train, validation, test)
+    using a prompt specification and saves them to a temporary
+    directory. Then, it checks that the dataset dict has the expected
+    keys, that each split has the expected number of examples, that
+    each dataset has the expected columns, that each example has the
+    expected empty values, and that the dataset dict is saved to the
+    output directory.
+
+    Returns:
+        None
+    """
     prompt_spec = None
     num_examples = {DatasetSplit.TRAIN: 10, DatasetSplit.VAL: 5, DatasetSplit.TEST: 3}
 
     dataset_generator = MockDatasetGenerator()
     with tempfile.TemporaryDirectory() as tmpdirname:
         output_dir = os.path.join(tmpdirname, "output")
-        dataset_dict = dataset_generator.generate_datasets(prompt_spec, num_examples, output_dir)
+        dataset_dict = dataset_generator.generate_datasets(
+            prompt_spec, num_examples, output_dir
+        )
 
         # Check that the dataset dict has the expected keys.
         assert set(dataset_dict.keys()) == {"train", "val", "test"}
@@ -54,4 +78,9 @@ def test_generate_datasets():
 
         # Check that the dataset dict is saved to the output directory.
         assert os.path.isdir(output_dir)
-        assert set(os.listdir(output_dir)) == {'dataset_dict.json', 'test', 'train', 'val'}
+        assert set(os.listdir(output_dir)) == {
+            "dataset_dict.json",
+            "test",
+            "train",
+            "val",
+        }
