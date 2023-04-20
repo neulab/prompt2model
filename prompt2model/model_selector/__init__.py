5e6a55ac566822e6904d985f02c97f3c35237e8f
viswavi <vijayv@andrew.cmu.edu>
2023-04-20 11:58:07 -0400

    Rename model selector -> param_selector

--- prompt2model/model_selector/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Import model selector classes."""
-from prompt2model.model_selector.base import ModelSelector
-from prompt2model.model_selector.mock import MockModelSelector
-
-__all__ = ("MockModelSelector", "ModelSelector")
