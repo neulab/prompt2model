2ca0e3100462b678117a0e82c5b0dba4d6d009af
zhaochen20 <zhaochenyang20@gmail.com>
2023-04-21 22:32:10 +0800

    retrain the final layer

--- prompt2model/trainer/simple.py
+++ prompt2model/trainer/simple.py
@@ -1,40 +1,28 @@
-"""This module provides a dummy trainer for testing purposes."""
+"""This module provides a simple trainer."""
 
 from pathlib import Path
 from typing import Any
 
 import datasets
 import transformers
-from torch.utils.data import DataLoader
-from transformers import HfArgumentParser, TrainingArguments, AutoModel, AutoTokenizer, PreTrainedTokenizer
+from torch.utils.data import ConcatDataset, DataLoader
 
 from prompt2model.trainer import Trainer
 
 
 class SimpleTrainer(Trainer):
-    """This is the simple trainer does not actually train anything."""
-
-    def __init__(self, pretrained_model_name: str):
-        """Initialize a simple model trainer.
-
-        Args:
-            pretrained_model_name: A HuggingFace model name to use for training.
-        """
-        self.model = AutoModel.from_pretrained(pretrained_model_name)
-        self.tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name)
-        self.wandb = None
-
-    def set_up_weights_and_biases(self) -> None:
-        """Set up Weights & Biases logging."""
-        self.wandb = None
-        raise NotImplementedError
+    """This is a simple trainer."""
 
     def train_model(
         self,
         training_datasets: list[datasets.Dataset],
         hyperparameter_choices: dict[str, Any],
     ) -> transformers.PreTrainedModel:
-        """Train a sequence classification model (TODO(Chenyang): update).
+        """Train a sequence classification model.
+
+        Use the self.model as a pretrained feature extractor
+        retrain the final layer from scratch to To solve the
+        inconsistency of pretraining and finetuning.
 
         Args:
             training_datasets: A list of training datasets.
@@ -43,77 +31,48 @@ class SimpleTrainer(Trainer):
         Returns:
             A trained HuggingFace model.
         """
-        # Set up checkpointing
-        output_dir = Path(hyperparameter_choices.get("output_dir", "./output"))
-        output_dir.mkdir(parents=True, exist_ok=True)
-        checkpoint_file = hyperparameter_choices.get("checkpoint_file")
-        if checkpoint_file is not None:
-            checkpoint_dict = hyperparameter_choices.copy()
-            checkpoint_dict["output_dir"] = str(output_dir)
-            parser = HfArgumentParser(TrainingArguments)
-            checkpoint = parser.parse_dict(checkpoint_dict)
-            model = transformers.AutoModelForSequenceClassification.from_pretrained(
-                checkpoint_file
-            )
-            optimizer = transformers.AdamW(
-                model.parameters(), lr=checkpoint.learning_rate
-            )
-            start_epoch = checkpoint.epoch + 1
-            global_step = checkpoint.global_step
-        else:
-            # Load the tokenizer and model
-            tokenizer = transformers.AutoTokenizer.from_pretrained(
-                hyperparameter_choices["model"]
-            )
-            model = transformers.AutoModelForSequenceClassification.from_pretrained(
-                hyperparameter_choices["model"]
-            )
 
-            # Set up the optimizer
-            optimizer = transformers.AdamW(
-                model.parameters(), lr=hyperparameter_choices["learning_rate"]
-            )
+        # Extract hyperparameters
+        batch_size = hyperparameter_choices["batch_size"]
+        num_epochs = hyperparameter_choices["num_epochs"]
+        learning_rate = hyperparameter_choices["learning_rate"]
+        device = hyperparameter_choices["device"]
 
-            checkpoint = TrainingArguments(output_dir=output_dir)
-            start_epoch = 0
-            global_step = 0
+        # Concatenate all training datasets
+        concatenated_dataset = ConcatDataset(training_datasets)
 
-        # Train the model
-        batch_size = hyperparameter_choices.get("batch_size", 32)
-        train_data_loader = DataLoader(
-            training_datasets, batch_size=batch_size, shuffle=True
+        # Create dataloader
+        train_dataloader = DataLoader(concatenated_dataset, batch_size=batch_size, shuffle=True)
+
+        # Clear all parameters in the final layer and retrain it
+        self.model.classifier = transformers.Identity()
+        self.model.classifier.out_proj = transformers.Linear(self.model.config.hidden_size, self.model.config.num_labels)
+
+        # Set up optimizer and scheduler
+        optimizer = transformers.AdamW(self.model.parameters(), lr=learning_rate)
+        scheduler = transformers.get_linear_schedule_with_warmup(
+            optimizer,
+            num_warmup_steps=0,
+            num_training_steps=len(train_dataloader) * num_epochs,
         )
 
-        for epoch in range(start_epoch, checkpoint.num_train_epochs):
-            for batch in train_data_loader:
-                inputs = tokenizer(
-                    batch["input_ids"],
-                    batch["attention_mask"],
-                    padding=True,
-                    truncation=True,
-                    return_tensors="pt",
-                )
-                labels = batch["label"]
+        # Set device and precision
+        self.model.to(device)
+        self.model.train()
+        transformers.set_seed(42)
 
-                outputs = model(**inputs, labels=labels)
+        # Train the model
+        for epoch in range(num_epochs):
+            for step, batch in enumerate(train_dataloader):
+                batch = {k: v.to(device) for k, v in batch.items()}
+
+                outputs = self.model(**batch)
                 loss = outputs.loss
                 loss.backward()
 
                 optimizer.step()
+                scheduler.step()
+                optimizer.zero_grad()
 
-                # Save checkpoint every save_steps steps
-                if (global_step + 1) % checkpoint.save_steps == 0:
-                    checkpoint.epoch = epoch
-                    checkpoint.global_step = global_step + 1
-                    checkpoint_dir = output_dir / f"checkpoint-{global_step + 1}"
-                    checkpoint_dir.mkdir(parents=True, exist_ok=True)
-                    model.save_pretrained(checkpoint_dir)
-                    checkpoint.save((checkpoint_dir / "checkpoint").as_posix())
-
-                global_step += 1
-
-        # Save final model if checkpointing was enabled
-        if checkpoint_file is None:
-            model.save_pretrained(output_dir)
-
-        return model
+        # Return the trained model
+        return self.model
