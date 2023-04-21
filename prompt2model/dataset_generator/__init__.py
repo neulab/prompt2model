0a1e2684edc78ce74e9bdedcd1f9f58c1bf9872a
zhaochen20 <zhaochenyang20@gmail.com>
2023-04-21 22:01:43 +0800

    add new interface

--- prompt2model/dataset_generator/__init__.py
+++ prompt2model/dataset_generator/__init__.py
@@ -1,5 +1,11 @@
 """Import DatasetGenerator classes."""
 from prompt2model.dataset_generator.base import DatasetGenerator, DatasetSplit
 from prompt2model.dataset_generator.mock import MockDatasetGenerator
+from prompt2model.dataset_generator.simple import SimpleDatasetGenerator
 
-__all__ = ("MockDatasetGenerator", "DatasetGenerator", "DatasetSplit")
+__all__ = (
+    "SimpleDatasetGenerator",
+    "MockDatasetGenerator",
+    "DatasetGenerator",
+    "DatasetSplit",
+)
