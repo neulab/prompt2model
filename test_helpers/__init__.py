8d067bccec354762038afd22345f54a80519522d
Vijay Viswanathan <vijayv@andrew.cmu.edu>
2023-04-29 13:10:22 -0400

    Update test_helpers/__init__.py
    Co-authored-by: Eren Chenyang Zhao <74843776+zhaochenyang20@users.noreply.github.com>

--- test_helpers/__init__.py
+++ test_helpers/__init__.py
@@ -1,4 +1,4 @@
-"""Import Trainer classes."""
+"""Import mock classes used in unit tests."""
 from test_helpers.mock_openai import MockCompletion, mock_openai_response
 
 __all__ = ("MockCompletion", "mock_openai_response")
