84db528dfcb9e88def118dc9c4d56dd54914b6f2
Vijay Viswanathan <vijayv@andrew.cmu.edu>
2023-04-29 22:57:32 -0400

    Refactor "Mock OpenAI" classes into a `test_helpers` directory (#29)
    * Rename test files to follow Pytest conventions
    
    * Move OpenAI mock classes to a test helpers directory
    
    * Use global path for pytest options in pyproject.toml
    
    * Add init file for test_helpers
    
    * Clean up __init__ file
    
    * Update test_helpers/__init__.py
    
    Co-authored-by: Eren Chenyang Zhao <74843776+zhaochenyang20@users.noreply.github.com>
    
    * Update test_helpers/mock_openai.py
    
    Co-authored-by: Eren Chenyang Zhao <74843776+zhaochenyang20@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Eren Chenyang Zhao <74843776+zhaochenyang20@users.noreply.github.com>

--- tests/test_dataset_generator.py
+++ tests/test_dataset_generator.py
@@ -1,6 +1,5 @@
 """Testing DatasetGenerator through SimpleDatasetGenerator."""
 
-import json
 import os
 import tempfile
 from functools import partial
@@ -9,56 +8,13 @@ from unittest.mock import patch
 from prompt2model.dataset_generator.base import DatasetSplit
 from prompt2model.dataset_generator.openai import OpenAIDatasetGenerator
 from prompt2model.prompt_parser import MockPromptSpec
-
-
-class MockCompletion:
-    """Mock openai completion object."""
-
-    def __init__(self, content: str):
-        """Initialize a new instance of MockCompletion class.
-
-        Args:
-            content: The mocked content to be returned, i.e.,
-            json.dumps({"comment": "This is a great movie!",
-            "label": 1}).
-        """
-        self.choices = [{"message": {"content": content}}]
-
-    def __repr__(self):
-        """Return a string representation of the MockCompletion object.
-
-        Returns:
-            _string: A string representation of the object, including its choices.
-        """
-        _string = f"<MockObject choices={self.choices}>"
-        return _string
-
-
-def mock_example(prompt: str, content: dict) -> MockCompletion:
-    """Generate a mock completion object containing a choice with example content.
-
-    This function creates a `MockCompletion` object with a `content` attribute set to
-    a JSON string representing an example label and comment. The `MockCompletion`
-    object is then returned.
-
-    Args:
-        prompt: A mocked prompt that won't be used.
-        content: The example content to be returned.
-
-    Returns:
-        a `MockCompletion` object.
-    """
-    _ = prompt
-    example_content = json.dumps(content)
-    mock_completion = MockCompletion(content=example_content)
-    return mock_completion
-
+from test_helpers import mock_openai_response
 
 mock_classification_example = partial(
-    mock_example, content={"sample": "This is a great movie!", "annotation": 1}
+    mock_openai_response, content={"sample": "This is a great movie!", "annotation": 1}
 )
 mock_translation_example = partial(
-    mock_example, content={"sample": "我爱你", "annotation": "I love you."}
+    mock_openai_response, content={"sample": "我爱你", "annotation": "I love you."}
 )
 
 
