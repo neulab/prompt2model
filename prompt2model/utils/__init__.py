d4d7bc5c78a52c708a425caf4aae0781ffd364fa
viswavi <vijayv@andrew.cmu.edu>
2023-04-30 16:20:49 -0400

    Move the generate_openai_chat_completion into a class

--- prompt2model/utils/__init__.py
+++ prompt2model/utils/__init__.py
@@ -1,5 +1,5 @@
 """Import utility functions."""
+from prompt2model.utils.openai_tools import ChatGPTAgent
 from prompt2model.utils.rng import seed_generator
-from prompt2model.utils.openai_tools import generate_openai_chat_completion
 
-__all__ = ("seed_generator", "generate_openai_chat_completion")
+__all__ = ("seed_generator", "ChatGPTAgent")
