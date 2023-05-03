7fe55b1b08b50fd99496d6986a5952d255a4a0be
zhaochen20 <zhaochenyang20@gmail.com>
2023-05-03 14:32:53 +0800

    init processer

---
+++ prompt2model/dataset_processer/base.py
@@ -0,0 +1,23 @@
+"""An interface for dataset retrieval."""
+
+from abc import ABC, abstractmethod
+
+import datasets
+
+from prompt2model.prompt_parser import PromptSpec
+
+
+# pylint: disable=too-few-public-methods
+class BasePrcesser(ABC):
+    """A class for post-processing datasets."""
+
+    @abstractmethod
+    def process_datasets(self, prompt_spec: PromptSpec) -> list[datasets.Dataset]:
+        """Retrieve datasets from a prompt specification.
+
+        Args:
+            prompt_spec: A prompt spec (containing a system description).
+
+        Returns:
+            A list of retrieved datasets.
+        """
