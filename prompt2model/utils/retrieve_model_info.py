9ebc512ed5d91eac64ae9781bb4218a9f85b2171
zhaochen20 <zhaochenyang20@gmail.com>
2023-05-09 09:22:40 +0800

    raise error

--- prompt2model/utils/retrieve_model_info.py
+++ prompt2model/utils/retrieve_model_info.py
@@ -34,17 +34,25 @@ def main(pretrained_model_name: str, cache_dir: str = None) -> None:
 
     model_bin_file = Path(f"{model_name}/pytorch_model.bin")
 
-    if model_bin_file.exists():
-        with open(model_bin_file, "r") as file:
-            content = file.read()
-            size = re.search(r"size (\d+)", content).group(1)  # type: ignore
-            print(size)
-    else:
-        model_bin_file = Path(f"{model_name}/pytorch_model.bin.index.json")
-        with open(model_bin_file, "r") as file:
-            content = json.loads(file.read())  # type: ignore
-            size = content["metadata"]["total_size"]  # type: ignore
-            print(size)
+    try:
+        if model_bin_file.exists():
+            with open(model_bin_file, "r") as file:
+                content = file.read()
+                size = re.search(r"size (\d+)", content).group(1)  # type: ignore
+                print(size)
+        else:
+            model_bin_file = Path(f"{model_name}/pytorch_model.bin.index.json")
+            with open(model_bin_file, "r") as file:
+                content = json.loads(file.read())  # type: ignore
+                size = content["metadata"]["total_size"]  # type: ignore
+                print(size)
+    except (
+        FileNotFoundError,
+        PermissionError,
+        IOError,
+        json.decoder.JSONDecodeError,
+    ) as e:
+        raise Exception(f"Failed to read {model_name} in {model_bin_file}: {e}")
 
     with open(f"{model_name}/README.md", "r", encoding="utf-8") as f:
         readme_content = f.read()
@@ -58,7 +66,7 @@ def main(pretrained_model_name: str, cache_dir: str = None) -> None:
     model_info = {
         "pretrained_model_name": pretrained_model_name,
         "description": readme_content,
-        "size": size,
+        "size_bytes": size,
         "downloads": downloads,
     }
     model_info_path = Path(f"{cache_dir}/{model_name}.json")
@@ -66,6 +74,8 @@ def main(pretrained_model_name: str, cache_dir: str = None) -> None:
     with open(model_info_path, "w") as file:
         file.write(json.dumps(model_info))
 
+    # The model must exist because it can be found by
+    # `model_meta = api.model_info(pretrained_model_name)`
     subprocess.run(["rm", "-rf", model_name])
 
 
