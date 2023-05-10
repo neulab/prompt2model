fb8dba668c1eb98067bd8cdb04790e7a93ecccf4
zhaochen20 <zhaochenyang20@gmail.com>
2023-05-10 09:58:21 +0800

    create static method

--- prompt2model/dataset_processor/base.py
+++ prompt2model/dataset_processor/base.py
@@ -24,26 +24,26 @@ class BaseProcessor(ABC):
         """
 
 
-def post_process_example(example: dict, instruction: str, task_id: int) -> dict:
-    """Modifies the input column of a given example dictionary.
-
-    Args:
-        example: A dictionary representing an example.
-        instruction: The instruction to convert task into a text2text generation.
-        task_id: The dataset index in dataset_dicts, typical for multi-task training.
-
-    Returns:
-        A dictionary representing the modified example.
-    """
-    assert (
-        "input_col" in example and "output_col" in example
-    ), "Example dictionary must have 'input_col' and 'output_col' keys"
-    example["input_col"] = f"<task {task_id}> {instruction} {example['input_col']}"
-    return example
-
-
 class DatasetProcessor(BaseProcessor):
     """A post-processing class for datasets, converting task into text2text fashion."""
+
+    @staticmethod
+    def post_process_example(example: dict, instruction: str, task_id: int) -> dict:
+        """Modifies the input column of a given example dictionary.
+
+        Args:
+            example: A dictionary representing an example.
+            instruction: The instruction to convert task into a text2text generation.
+            task_id: The dataset index in dataset_dicts, typical for multi-task training.
+
+        Returns:
+            A dictionary representing the modified example.
+        """
+        assert (
+            "input_col" in example and "output_col" in example
+        ), "Example dictionary must have 'input_col' and 'output_col' keys"
+        example["input_col"] = f"<task {task_id}> {instruction} {example['input_col']}"
+        return example
 
     def process_dataset_dict(
         self, instruction: str, dataset_dicts: list[datasets.DatasetDict]
@@ -60,7 +60,7 @@ class DatasetProcessor(BaseProcessor):
         modified_dataset_dicts = []
         for task_id, dataset_dict in enumerate(dataset_dicts):
             mapping_function = partial(
-                post_process_example, instruction=instruction, task_id=task_id
+                self.post_process_example, instruction=instruction, task_id=task_id
             )
             modified_dataset_dict = dataset_dict.map(mapping_function)
             modified_dataset_dicts.append(modified_dataset_dict)
