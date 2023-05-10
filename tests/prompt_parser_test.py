45f99737302a6378ff02909670ab0c77af1cc85f
viswavi <vijayv@andrew.cmu.edu>
2023-05-09 22:59:34 -0400

    Add tests for various OpenAI API failures

--- tests/prompt_parser_test.py
+++ tests/prompt_parser_test.py
@@ -1,7 +1,11 @@
 """Testing integration of components locally."""
 
+import json
 from functools import partial
 from unittest.mock import patch
+
+import openai
+import pytest
 
 from prompt2model.prompt_parser import OpenAIInstructionParser, TaskType
 from test_helpers import mock_openai_response
@@ -11,7 +15,6 @@ GPT3_RESPONSE_WITH_DEMONSTRATIONS = (
     ' MM/DD/YYYY format.", "Demonstrations": "Fifth of November 2024 ->'
     ' 11/05/2024\nJan. 9 2023 -> 01/09/2023\nChristmas 2016 -> 12/25/2016"}'
 )
-
 GPT3_RESPONSE_WITHOUT_DEMONSTRATIONS = (
     '{"Instruction": "Turn the given fact into a question by a simple rearrangement'
     " of words. This typically involves replacing some part of the given fact with a"
@@ -24,12 +27,25 @@ GPT3_RESPONSE_WITHOUT_DEMONSTRATIONS = (
     " You can also form a question without any WH words. For example, 'A radio"
     ' converts electricity into?\'", "Demonstrations": "N/A"}'
 )
+GPT3_RESPONSE_WITH_INVALID_JSON = (
+    '{"Instruction": "A", "Demonstrations": "B}'  # Missing final quotation mark
+)
+
+
+class UNKNOWN_GPT3_EXCEPTION(Exception):
+    """This is a newly-defined exception for testing purposes."""
+
+    pass
+
 
 mock_prompt_parsing_example_with_demonstrations = partial(
     mock_openai_response, content=GPT3_RESPONSE_WITH_DEMONSTRATIONS
 )
 mock_prompt_parsing_example_without_demonstrations = partial(
     mock_openai_response, content=GPT3_RESPONSE_WITHOUT_DEMONSTRATIONS
+)
+mock_prompt_parsing_example_with_invalid_json = partial(
+    mock_openai_response, content=GPT3_RESPONSE_WITH_INVALID_JSON
 )
 
 
@@ -84,4 +100,71 @@ def test_instruction_parser_without_demonstration(mocked_parsing_method):
         == "Turn the given fact into a question by a simple rearrangement of words. This typically involves replacing some part of the given fact with a WH word. For example, replacing the subject of the provided fact with the word \"what\" can form a valid question. Don't be creative! You just need to rearrange the words to turn the fact into a question - easy! Don't just randomly remove a word from the given fact to form a question. Remember that your question must evaluate scientific understanding. Pick a word or a phrase in the given fact to be the correct answer, then make the rest of the question. You can also form a question without any WH words. For example, 'A radio converts electricity into?'"  # noqa: E501
     )
     assert prompt_spec.demonstration == "N/A"
+    assert mocked_parsing_method.call_count == 1
+
+
+@patch(
+    "prompt2model.utils.ChatGPTAgent.generate_openai_chat_completion",
+    side_effect=mock_prompt_parsing_example_with_invalid_json,
+)
+def test_instruction_parser_with_invalid_json(mocked_parsing_method):
+    """Verify that we handle when the API returns a invalid JSON response.
+
+    Args:
+        mocked_parsing_method: Mocked function for parsing a prompt using GPT.
+    """
+    prompt = """This prompt will be ignored by the parser in this test."""
+    with pytest.raises(json.decoder.JSONDecodeError):
+        prompt_spec = OpenAIInstructionParser(
+            task_type=TaskType.TEXT_GENERATION, max_api_calls=3
+        )
+        prompt_spec.parse_from_prompt(prompt)
+
+    assert mocked_parsing_method.call_count == 3
+
+
+@patch("time.sleep")
+@patch(
+    "prompt2model.utils.ChatGPTAgent.generate_openai_chat_completion",
+    side_effect=openai.error.Timeout("timeout"),
+)
+def test_instruction_parser_with_timeout(mocked_parsing_method, mocked_sleep_method):
+    """Verify that we wait and retry (a set number of times) if the API times out.
+
+    Args:
+        mocked_parsing_method: Mocked function for parsing a prompt using GPT.
+        mocked_sleep_method: Mock of `time.sleep` to avoid actually waiting, to
+                             verify that we wait before each retry.
+    """
+    prompt = """This prompt will be ignored by the parser in this test."""
+    with pytest.raises(openai.error.Timeout):
+        prompt_spec = OpenAIInstructionParser(
+            task_type=TaskType.TEXT_GENERATION, max_api_calls=3
+        )
+        prompt_spec.parse_from_prompt(prompt)
+
+    # If we allow 3 API calls, we should have 2 sleep calls (1 after each
+    # timeout).
+    assert mocked_sleep_method.call_count == 2
+    assert mocked_parsing_method.call_count == 3
+
+
+@patch(
+    "prompt2model.utils.ChatGPTAgent.generate_openai_chat_completion",
+    side_effect=UNKNOWN_GPT3_EXCEPTION(),
+)
+def test_instruction_parser_with_unexpected_error(mocked_parsing_method):
+    """Verify we don't retry the API call if an unexpected exception appears.
+
+    Args:
+        mocked_parsing_method: Mocked function for parsing a prompt using GPT.
+    """
+    prompt = """This prompt will be ignored by the parser in this test."""
+    with pytest.raises(UNKNOWN_GPT3_EXCEPTION):
+        prompt_spec = OpenAIInstructionParser(
+            task_type=TaskType.TEXT_GENERATION, max_api_calls=3
+        )
+        prompt_spec.parse_from_prompt(prompt)
+
+    # Check that we only tried calling the API once.
     assert mocked_parsing_method.call_count == 1
