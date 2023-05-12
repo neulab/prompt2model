2d3fb0cfe8976c1367f414ff9b302d430f7bd367
zhaochen20 <zhaochenyang20@gmail.com>
2023-05-12 21:47:47 +0800

    detach generator trainer

--- prompt2model/model_trainer/__init__.py
+++ prompt2model/model_trainer/__init__.py
@@ -1,5 +1,6 @@
 """Import BaseTrainer classes."""
-from prompt2model.model_trainer.base import BaseTrainer, ModelTrainer
+from prompt2model.model_trainer.base import BaseTrainer
+from prompt2model.model_trainer.generate import GenerationModelTrainer
 from prompt2model.model_trainer.mock import MockTrainer
 
-__all__ = ("MockTrainer", "BaseTrainer", "T5Trainer", "ModelTrainer")
+__all__ = ("MockTrainer", "BaseTrainer", "T5Trainer", "GenerationModelTrainer")
--- tests/model_trainer_test.py
+++ tests/model_trainer_test.py
@@ -1,28 +1,29 @@
-"""Testing ModelTrainer with different configurations."""
+"""Testing GenerationModelTrainer with different configurations."""
 
 import tempfile
 
 import datasets
 import transformers
 
-from prompt2model.model_trainer.base import ModelTrainer
+from prompt2model.model_trainer.generate import GenerationModelTrainer
 
 
 def test_trainer():
-    """Test the ModelTrainer class.
+    """Test the GenerationModelTrainer class.
 
-    This function tests the ModelTrainer class by training two different models using
-    different configurations. The first model is an encoder-decoder model implemented
-    with the T5 architecture, and the second is a decoder-only model implemented with
-    the GPT-2 architecture. The function creates temporary directories to store the
-    trained models and their respective tokenizers. It then creates two training
-    datasets with synthetic data, one for each model. Finally, it trains each model
-    using the ModelTrainer class and tests whether the output models and tokenizers
-    are of the expected type.
+    This function tests the GenerationModelTrainer class by training two
+    different models using different configurations. The first model is an
+    encoder-decoder model implemented with the T5 architecture, and the
+    second is a decoder-only model implemented with the GPT-2 architecture.
+    The function creates temporary directories to store the trained models
+    and their respective tokenizers. It then creates two training datasets
+    with synthetic data, one for each model. Finally, it trains each model
+    using the GenerationModelTrainer class and tests whether the output
+    models and tokenizers are of the expected type.
     """
-    # Test encoder-decoder ModelTrainer implementation
+    # Test encoder-decoder GenerationModelTrainer implementation
     with tempfile.TemporaryDirectory() as cache_dir:
-        trainer = ModelTrainer("t5-small", has_encoder=True)
+        trainer = GenerationModelTrainer("t5-small", has_encoder=True)
         training_datasets = [
             datasets.Dataset.from_dict(
                 {
@@ -40,9 +41,9 @@ def test_trainer():
         assert isinstance(trained_model, transformers.T5ForConditionalGeneration)
         assert isinstance(trained_tokenizer, transformers.T5Tokenizer)
 
-    # Test decoder-only ModelTrainer implementation
+    # Test decoder-only GenerationModelTrainer implementation
     with tempfile.TemporaryDirectory() as cache_dir:
-        trainer = ModelTrainer("gpt2", has_encoder=False)
+        trainer = GenerationModelTrainer("gpt2", has_encoder=False)
         training_datasets = [
             datasets.Dataset.from_dict(
                 {
