a551b90f83bc32357731a8bd1cde20bc72773895
zhaochen20 <zhaochenyang20@gmail.com>
2023-05-12 10:27:36 +0800

    restore the  dataset generator

--- prompt2model/dataset_generator/openai_gpt.py
+++ prompt2model/dataset_generator/openai_gpt.py
@@ -1,138 +1,124 @@
-"""A simple dataset generator that uses OpenAI's GPT-3.5 API."""
+"""Testing DatasetGenerator through SimpleDatasetGenerator."""
 
-import json
-import logging
+import os
+import tempfile
+from functools import partial
+from unittest.mock import patch
 
-import openai
-from datasets import Dataset
-from tqdm import tqdm
+from prompt2model.dataset_generator.base import DatasetSplit
+from prompt2model.dataset_generator.openai_gpt import OpenAIDatasetGenerator
+from prompt2model.prompt_parser import MockPromptSpec
+from test_helpers import mock_openai_response
 
-from prompt2model.dataset_generator.base import DatasetGenerator, DatasetSplit
-from prompt2model.prompt_parser import PromptSpec
-from prompt2model.utils import OPENAI_ERRORS, ChatGPTAgent, handle_openai_error
+mock_classification_example = partial(
+    mock_openai_response,
+    content='{"sample": "This is a great movie!", "annotation": 1}',
+)
+mock_translation_example = partial(
+    mock_openai_response, content='{"sample": "我爱你", "annotation": "I love you."}'
+)
 
 
-class OpenAIDatasetGenerator(DatasetGenerator):
-    """A abstract class for NLP dataset generation using OpenAI's GPT-3.5 API."""
+def check_generate_dataset(dataset_generator: OpenAIDatasetGenerator):
+    """Test the `generate_dataset_split()` function of `OpenAIDatasetGenerator`.
 
-    def __init__(self, api_key: str, max_api_calls: int = None):
-        """Initialize an OpenAI DatasetGenertor with an API key and max API call.
+    This function generates a Dataset for a specified split of the data
+    (train, validation, or test) using a simple prompt specification
+    and saves them to a temporary directory. Then, it checks that the
+    generated dataset has the expected number of examples, the expected
+    columns, and each example is not empty.
+    """
+    prompt_spec = MockPromptSpec()
+    num_examples = 1
+    split = DatasetSplit.TRAIN
+    dataset = dataset_generator.generate_dataset_split(prompt_spec, num_examples, split)
 
-        Args:
-            api_key: A valid OpenAI API key.
-            max_api_calls: The maximum number of API calls allowed,
-                or None for unlimited.
-        """
-        self.api_key = api_key
-        self.max_api_calls = max_api_calls
-        self.api_call_counter = 0
+    # Check that the generated dataset has the expected number of examples.
+    assert len(dataset) == num_examples
 
-    def generate_prompt(
-        self,
-        instruction: str,
-        examples: list[str] = None,
-        prompt_template: str = None,
-    ) -> str:
-        """Generates a prompt string.
+    # Check that the generated dataset has the expected columns.
+    expected_columns = {"input_col", "output_col"}
+    assert set(dataset.column_names) == expected_columns
 
-        Args:
-            instruction: The natural language instruction for the prompt.
-            examples: A list of few-shot examples. Defaults to None.
-            prompt_template: A string template for the prompt. Defaults to None.
-                Prompt_template must contains `instruction` and `examples` fields.
+    # Check that each example is not empty.
+    for example in dataset:
+        assert example["output_col"] != "", "Expected example to not be empty"
 
-        Returns:
-            The generated prompt string.
-        """
-        # Set default prompt template if not provided
-        if not prompt_template:
-            prompt_template = (
-                "Requirement: {instruction} \n"
-                "Few-Shot Examples: {examples} \n"
-                "sample: \n"
-                "annotation: \n"
-                "Please answer me in JSON format, with `sample` and `annotation` keys."
+
+def check_generate_dataset_dict(dataset_generator: OpenAIDatasetGenerator):
+    """Test the `generate_dataset_dict()` function of `OpenAIDatasetGenerator`.
+
+    This function generates movie comments datasets by creating a specified
+    number of examples for each split of the data, which includes
+    train, validation, and test. It uses a simple prompt
+    specification and saves the generated datasets to a temporary
+    directory. Afterward, the function checks whether the dataset
+    dictionary contains all the expected keys, each split has the
+    anticipated number of examples, every dataset has the anticipated
+    columns, each example is not empty, and whether the dataset dictionary
+    is saved to the output directory.
+    """
+    prompt_spec = MockPromptSpec()
+    num_examples = {DatasetSplit.TRAIN: 1, DatasetSplit.VAL: 1, DatasetSplit.TEST: 0}
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        output_dir = os.path.join(tmpdirname, "output")
+        dataset_dict = dataset_generator.generate_dataset_dict(
+            prompt_spec=prompt_spec, num_examples=num_examples, output_dir=output_dir
+        )
+
+        assert set(dataset_dict.keys()) == {"train", "val", "test"}
+        for split, num in num_examples.items():
+            assert len(dataset_dict[split.value]) == num, (
+                f"Expected {num} examples for {split.value} split, but"
+                f" got {len(dataset_dict[split.value])}"
             )
+        expected_columns = {"input_col", "output_col"}
+        for dataset in dataset_dict.values():
+            assert (
+                set(dataset.column_names) == expected_columns
+            ), f"Expected columns {expected_columns}, but got {dataset.column_names}"
+            for example in dataset:
+                assert example["output_col"] != "", "Expected example to not be empty"
+        assert os.path.isdir(output_dir)
+        assert set(os.listdir(output_dir)) == {
+            "dataset_dict.json",
+            "test",
+            "train",
+            "val",
+        }
 
-        # Replace placeholders in prompt template with actual values
-        example_string = " ".join(examples) if examples else "NA"
-        prompt = prompt_template.format(
-            instruction=instruction, examples=example_string
-        )
-        return prompt
 
-    def extract_response(self, response: openai.Completion) -> tuple[str, str]:
-        """Extracts the generated sample and annotation from an OpenAI API response.
+@patch(
+    "prompt2model.utils.ChatGPTAgent.generate_openai_chat_completion",
+    side_effect=mock_translation_example,
+)
+def test_translation_dataset_generation(mocked_generate_example):
+    """Test translation dataset generation using the OpenAIDatasetGenerator.
 
-        Args:
-            response (openai.Completion): The response object returned by OpenAI API.
+    Args:
+        mocked_generate_example: None used parameter.
+        But test_translation_dataset_generation should require one
+        positional argument.
+    """
+    api_key = None
+    dataset_generator = OpenAIDatasetGenerator(api_key)
+    check_generate_dataset_dict(dataset_generator)
+    check_generate_dataset(dataset_generator)
 
-        Returns:
-            A tuple of (sample, annotation), where:
-            - sample is the generated example string extracted from the response.
-            - annotation is the generated label string extracted from the response.
-        """
-        try:
-            response_json = json.loads(response.choices[0]["message"]["content"])
-        except json.decoder.JSONDecodeError as e:
-            logging.warning("API response was not a valid JSON")
-            raise e
-        required_keys = ["sample", "annotation"]
-        missing_keys = [key for key in required_keys if key not in response_json]
-        assert (
-            len(missing_keys) == 0
-        ), f'API response must contain {", ".join(required_keys)} keys'
-        sample = response_json["sample"].strip()
-        annotation = response_json["annotation"].strip()
-        return sample, annotation
 
-    def generate_dataset_split(
-        self,
-        prompt_spec: PromptSpec,
-        num_examples: int,
-        split: DatasetSplit,
-    ) -> Dataset:
-        """Generate a single dataset using GPT-3.5.
+@patch(
+    "prompt2model.utils.ChatGPTAgent.generate_openai_chat_completion",
+    side_effect=mock_classification_example,
+)
+def test_classification_dataset_generation(mocked_generate_example):
+    """Test classification dataset generation using the OpenAIDatasetGenerator.
 
-        Args:
-            prompt_spec: A prompt specification.
-            num_examples: Number of examples in split.
-            split: Name of dataset split to generate.
-
-        Returns:
-            A single dataset.
-        """
-        _ = split  # suppress unused variable warnings
-        prompt = self.generate_prompt(
-            instruction=prompt_spec.instruction,
-            examples=prompt_spec.examples,
-            prompt_template=prompt_spec.prompt_template,
-        )
-        chat_api = ChatGPTAgent(self.api_key)
-        input_cols = []  # type: list[str]
-        output_cols = []  # type: list[str]
-
-        for _ in tqdm(range(num_examples), desc="Generating examples"):
-            while True:
-                try:
-                    if (
-                        self.max_api_calls
-                        and self.api_call_counter >= self.max_api_calls
-                    ):
-                        logging.warning("Maximum number of API calls reached.")
-                        return Dataset.from_dict(
-                            {"input_col": input_cols, "output_col": output_cols}
-                        )
-                    else:
-                        self.api_call_counter += 1
-                    response = chat_api.generate_openai_chat_completion(prompt)
-                    input_col, output_col = self.extract_response(response)
-                    input_cols.append(input_col)
-                    output_cols.append(output_col)
-                    break
-                except OPENAI_ERRORS as e:
-                    self.api_call_counter = handle_openai_error(
-                        e, self.api_call_counter, self.max_api_calls
-                    )
-
-        return Dataset.from_dict({"input_col": input_cols, "output_col": output_cols})
+    Args:
+        mocked_generate_example: None used parameter.
+        But test_classification_dataset_generation should require one
+        positional argument.
+    """
+    api_key = None
+    dataset_generator = OpenAIDatasetGenerator(api_key)
+    check_generate_dataset_dict(dataset_generator)
+    check_generate_dataset(dataset_generator)
