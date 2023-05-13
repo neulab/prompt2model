d32ac4a578f00270ab3c806558dcee2f52a37690
Eren Chenyang Zhao <74843776+zhaochenyang20@users.noreply.github.com>
2023-05-13 08:42:48 +0800

    implement dataset processor (#48)
    * init processer
    
    * rename dataset_dict
    
    * delecte pd
    
    * create DatasetProcessor
    
    * create unit test for DatasetProcessor
    
    * create unit test for DatasetProcessor
    
    * rename model id to model name
    
    * Update prompt2model/dataset_generator/base.py
    
    Co-authored-by: Vijay Viswanathan <vijayv@andrew.cmu.edu>
    
    * Update prompt2model/dataset_retriever/base.py
    
    Co-authored-by: Vijay Viswanathan <vijayv@andrew.cmu.edu>
    
    * Update prompt2model/dataset_generator/base.py
    
    Co-authored-by: Vijay Viswanathan <vijayv@andrew.cmu.edu>
    
    * delete wandb
    
    * rename processor
    
    * rename processor
    
    * rename processor
    
    * rename run_local
    
    * change local test
    
    * change local test
    
    * local test success
    
    * change pyproject.toml
    
    * change the define of instruction
    
    * change wordd name
    
    * change define of
    
    * create static method
    
    * create static method
    
    * init GPT processor
    
    * change document
    
    * reuse 3.11.3, combine t5 and gpt
    
    * new design for dataset processor
    
    * detach unit tests
    
    * fix typo
    
    ---------
    
    Co-authored-by: zhaochen20 <zhaochenyang20@gmail.com>
    Co-authored-by: Vijay Viswanathan <vijayv@andrew.cmu.edu>

--- prompt2model/dataset_processor/base.py
+++ prompt2model/dataset_processor/base.py
@@ -1,15 +1,40 @@
-"""An interface for dataset processor."""
+"""A base class for dataset processor."""
 
 from abc import ABC, abstractmethod
+from functools import partial
 
 import datasets
 
 
-# pylint: disable=too-few-public-methods
 class BaseProcessor(ABC):
-    """A class for post-processing datasets."""
+    """A base class for post-processing datasets."""
 
+    def __init__(self, has_encoder: bool) -> None:
+        """Initialize the `BaseProcessor`.
+
+        Args:
+            has_encoder: Whether the retrieved model has an encoder.
+                Encoder-decoder model like T5 has two model inputs.
+                Decoder-only model like GPT only has one model input, thus
+                `model_input` should be added with the `output_col`.
+        """
+        self.has_encoder = has_encoder
+
+    @staticmethod
     @abstractmethod
+    def post_process_example(
+        example: dict, instruction: str, task_id: int, has_encoder: bool
+    ) -> dict:
+        """Modifies the input column of a given example dictionary.
+
+        Args:
+            example: A dictionary representing an example.
+            instruction: The instruction used as a prefix to explain the task.
+            task_id: A tag marking which dataset (from dataset_dicts) this example
+                comes from. Used for multi-task training.
+            has_encoder: Whether the retrieved model has an encoder.
+        """
+
     def process_dataset_dict(
         self, instruction: str, dataset_dicts: list[datasets.DatasetDict]
     ) -> list[datasets.DatasetDict]:
@@ -22,3 +47,14 @@ class BaseProcessor(ABC):
         Returns:
             A list of DatasetDicts, all examples are converted into text2text fashion.
         """
+        modified_dataset_dicts = []
+        for task_id, dataset_dict in enumerate(dataset_dicts):
+            mapping_function = partial(
+                self.post_process_example,
+                instruction=instruction,
+                task_id=task_id,
+                has_encoder=self.has_encoder,
+            )
+            modified_dataset_dict = dataset_dict.map(mapping_function)
+            modified_dataset_dicts.append(modified_dataset_dict)
+        return modified_dataset_dicts
