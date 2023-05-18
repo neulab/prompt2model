b81d8ba6e3a199cef1c5b967fa10694194245fcc
zhaochen20 <zhaochenyang20@gmail.com>
2023-05-18 18:45:47 +0800

    change skeleton

--- prompt2model/prompt_parser/mock.py
+++ prompt2model/prompt_parser/mock.py
@@ -1,13 +1,14 @@
 """An interface for prompt parsing."""
 
-from prompt2model.prompt_parser.base import PromptSpec
+from prompt2model.prompt_parser.base import PromptSpec, TaskType
 
 
 class MockPromptSpec(PromptSpec):
     """Mock the bebavior of PromptSpec."""
 
-    def __init__(self):
+    def __init__(self, task_type: TaskType):
         """Mock the elements of PromptSpec."""
+        self.task_type = task_type
         self.instruction = (
             "Give me some translation from Chinese to English."
             " Input Chinese and output English."
@@ -26,8 +27,10 @@ class MockPromptSpec(PromptSpec):
 
     def parse_from_prompt(self, prompt: str) -> None:
         """Don't parse anything."""
+        _ = prompt
         return None
 
     def get_instruction(self, prompt: str) -> str:
         """Return the prompt itself, since we do not parse it."""
+        _ = prompt
         return self.instruction
