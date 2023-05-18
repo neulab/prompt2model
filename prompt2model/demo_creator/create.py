d7ae857d5c65efab782b17bf61692f3ec98bcb9d
zhaochen20 <zhaochenyang20@gmail.com>
2023-05-18 18:31:45 +0800

    add creator

--- prompt2model/demo_creator/create.py
+++ prompt2model/demo_creator/create.py
@@ -1,7 +1,4 @@
-"""An interface for creating Gradio demos automatically."""
-
 import gradio as gr
-
 from prompt2model.model_executor import GenerationModelExecutor
 from prompt2model.prompt_parser import OpenAIInstructionParser
 
@@ -19,29 +16,21 @@ def create_gradio(
         A Gradio interface for interacting with the model.
 
     """
-
-    def predict(input_text):
-        prompt_parser.parse_from_prompt(input_text)
+    def chat(message, history):
+        history = history or []
+        prompt_parser.parse_from_prompt(message)
         instruction = prompt_parser.get_instruction()
-
-        # Generate model outputs based on the instruction
         model_executor.set_instruction(instruction)
-        model_outputs = model_executor.make_predictions()
+        response = model_executor.make_single_prediction(message).prediction
+        history.append((message, response))
+        return history, history
 
-        # Extract predictions from model outputs
-        predictions = [output.prediction for output in model_outputs]
-        return predictions
-
-    # Define the Gradio interface input
-    interface_input = gr.inputs.Textbox(label="User Prompt")
-
-    # Define the Gradio interface output
-    interface_output = gr.outputs.Textbox(label="Model Predictions")
-
-    # Create the Gradio interface with the predict function
-    # and the defined inputs/outputs
     gr_interface = gr.Interface(
-        fn=predict, inputs=interface_input, outputs=interface_output
+        fn=chat,
+        inputs=["text", "text[]"],
+        outputs=["text[]", "text[]"],
+        allow_screenshot=False,
+        allow_flagging="never",
     )
 
     return gr_interface
