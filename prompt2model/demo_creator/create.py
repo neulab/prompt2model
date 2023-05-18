522973d4dfe7f8217d35ea90ad4380c196390d51
zhaochen20 <zhaochenyang20@gmail.com>
2023-05-18 15:43:33 +0800

    create new interface, need executor

--- prompt2model/demo_creator/create.py
+++ prompt2model/demo_creator/create.py
@@ -1,24 +1,42 @@
 """An interface for creating Gradio demos automatically."""
 
 import gradio as gr
-
-from prompt2model.model_executor import MockModelExecutor
-from prompt2model.prompt_parser import PromptSpec
-
+from prompt2model.prompt_parser import TaskType
+from prompt2model.model_executor import GenerationModelExecutor
+from prompt2model.prompt_parser import OpenAIInstructionParser
 
 def gradio_create(
-    model_executor: MockModelExecutor, prompt_spec: PromptSpec
+    model_executor: GenerationModelExecutor, prompt_parser: OpenAIInstructionParser
 ) -> gr.Interface:
     """Create a Gradio interface automatically.
 
     Args:
-        model: A trained model to expose via a Gradio interface.
-        prompt_spec: A PromptSpec to help choose the visual interface.
+        model_executor: A trained GenerationModelExecutor to expose via a Gradio interface.
+        prompt_parser: An instance of OpenAIInstructionParser to parse the prompt.
 
     Returns:
         A Gradio interface for interacting with the model.
 
     """
-    _ = model_executor, prompt_spec  # suppress unused variable warnings
-    dummy_interface = gr.Interface(lambda input: None, "textbox", "label")
-    return dummy_interface
+    def predict(input_text):
+        prompt_parser.parse_from_prompt(input_text)
+        instruction = prompt_parser.get_instruction()
+
+        # Generate model outputs based on the instruction
+        model_executor.set_instruction(instruction)
+        model_outputs = model_executor.make_predictions()
+
+        # Extract predictions from model outputs
+        predictions = [output.prediction for output in model_outputs]
+        return predictions
+
+    # Define the Gradio interface input
+    interface_input = gr.inputs.Textbox(label="User Prompt")
+
+    # Define the Gradio interface output
+    interface_output = gr.outputs.Textbox(label="Model Predictions")
+
+    # Create the Gradio interface with the predict function and the defined inputs/outputs
+    gr_interface = gr.Interface(fn=predict, inputs=interface_input, outputs=interface_output)
+
+    return gr_interface
