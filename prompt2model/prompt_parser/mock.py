1c7a73bf5095b7dcd636b3309020f5d0445735fd
zhaochen20 <zhaochenyang20@gmail.com>
2023-05-19 16:03:07 +0800

    detach refactor of prompt sepc

--- prompt2model/prompt_parser/mock.py
+++ prompt2model/prompt_parser/mock.py
@@ -1,21 +1,22 @@
 """An interface for prompt parsing."""
 
-from prompt2model.prompt_parser.base import PromptSpec
+from prompt2model.prompt_parser.base import PromptSpec, TaskType
 
 
 class MockPromptSpec(PromptSpec):
     """Mock the bebavior of PromptSpec."""
 
-    def __init__(self):
+    def __init__(self, task_type: TaskType):
         """Mock the elements of PromptSpec."""
+        self.task_type = task_type
         self.instruction = (
             "Give me some translation from Chinese to English."
             " Input Chinese and output English."
         )
-        self.examples = [
-            "input: '人生苦短，我用 Python', output: 'Life is short, I use Python.'",
-            "input: '明天是周末', output: 'Tomorrow is weekend.'",
-        ]
+        self.examples = (
+            "input: '人生苦短，我用 Python', output: 'Life is short, I use Python. '"
+            "input: '明天是周末', output: 'Tomorrow is weekend.'"
+        )
         self.prompt_template = (
             "Requirement: {instruction} \n"
             "Few-Shot Examples: {examples} \n"
@@ -26,8 +27,5 @@ class MockPromptSpec(PromptSpec):
 
     def parse_from_prompt(self, prompt: str) -> None:
         """Don't parse anything."""
+        self.instruction = prompt
         return None
-
-    def get_instruction(self, prompt: str) -> str:
-        """Return the prompt itself, since we do not parse it."""
-        return self.instruction
