180d48fa001d5ed62939070044bb0b68228b8134
Eren Chenyang Zhao <74843776+zhaochenyang20@users.noreply.github.com>
2023-05-20 00:06:45 +0800

    Update tests/demo_creator_test.py
    Co-authored-by: Vijay Viswanathan <vijayv@andrew.cmu.edu>

--- tests/demo_creator_test.py
+++ tests/demo_creator_test.py
@@ -58,7 +58,7 @@ def test_create_gradio_with_t5():
         batch_size=1,
     )
 
-    # Create OpenAIInstructionParser
+    # Create OpenAIInstructionParser.
     t5_prompt_parser = MockPromptSpec(task_type="generation")
 
     # Create Gradio interface.
