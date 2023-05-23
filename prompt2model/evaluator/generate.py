74f33596896ff37f98053fb054f59183335bdede
zhaochen20 <zhaochenyang20@gmail.com>
2023-05-24 07:57:07 +0800

    init seq2seq evaluator

--- prompt2model/evaluator/generate.py
+++ prompt2model/evaluator/generate.py
@@ -4,10 +4,9 @@ from prompt2model.evaluator.base import Evaluator
 from typing import Any
 
 import datasets
-
+from datasets import load_metric
 from prompt2model.model_executor import ModelOutput
 from prompt2model.prompt_parser import PromptSpec
-from prompt2model.model_executor import GenerationModelExecutor
 
 class Seq2SeqEvaluator(Evaluator):
     """An evaluator computing `ChrF++`, `Exact Match` and `Embedding Distance`."""
@@ -32,3 +31,37 @@ class Seq2SeqEvaluator(Evaluator):
         Returns:
             A dictionary of metric values to return.
         """
+        _ = prompt_spec
+        if metrics is None:
+            # Load the required metrics
+            metrics = [
+                load_metric("chrf"),
+                load_metric("exact_match"),
+                load_metric("embedding_distance"),
+            ]
+        # Get the ground truth from the dataset
+        ground_truth = dataset[gt_column]
+
+        # Extract the predicted strings from ModelOutput
+        predicted_strings = [each.prediction for each in predictions]
+
+        # Initialize the metric values dictionary
+        metric_values = {}
+
+        # Compute and store metric values
+        for metric in metrics:
+            metric_name = metric.name
+            if metric_name == "chrf":
+                references = ground_truth.tolist()
+                metric.add_batch(predictions=predicted_strings, references=references)
+                metric_values[metric_name] = metric.compute()
+            elif metric_name == "exact_match":
+                references = ground_truth.tolist()
+                metric.add_batch(predictions=predicted_strings, references=references)
+                metric_values[metric_name] = metric.compute()["exact_match"]
+            elif metric_name == "embedding_distance":
+                references = ground_truth.tolist()
+                metric.add_batch(predictions=predicted_strings, references=references)
+                metric_values[metric_name] = metric.compute()["distance"]
+
+        return metric_values
