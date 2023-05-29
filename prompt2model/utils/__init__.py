f08b2a7c5caca8c57ee0828d4c244b7af13edc0b
viswavi <vijayv@andrew.cmu.edu>
2023-05-28 23:31:23 -0400

    Add retrieve_objects functionality to tevatron utils

--- prompt2model/utils/__init__.py
+++ prompt2model/utils/__init__.py
@@ -5,7 +5,11 @@ from prompt2model.utils.openai_tools import (
     handle_openai_error,
 )
 from prompt2model.utils.rng import seed_generator
-from prompt2model.utils.tevatron_utils import encode_search_corpus, encode_text
+from prompt2model.utils.tevatron_utils import (
+    encode_search_corpus,
+    encode_text,
+    retrieve_objects,
+)
 
 __all__ = (  # noqa: F401
     "ChatGPTAgent",
--- prompt2model/utils/tevatron_utils/__init__.py
+++ prompt2model/utils/tevatron_utils/__init__.py
@@ -1,4 +1,5 @@
 """Import Tevatron utility functions."""
 from prompt2model.utils.tevatron_utils.encode import encode_search_corpus, encode_text
+from prompt2model.utils.tevatron_utils.retrieve import retrieve_objects
 
 __all__ = ("encode_search_corpus", "encode_text", "retrieve_objects")
