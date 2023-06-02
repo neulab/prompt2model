5beb4e1b750e81116dbee6b1b5d8d68335457832
Vijay Viswanathan <vijayv@andrew.cmu.edu>
2023-06-02 10:58:35 -0400

    Add retrieve function to `tevatron utils` (#81)
    * Add get_instruction method to PromptSpec interface
    
    * Pass lint
    
    * Utility library for Tevatron retrieval
    
    * Add tevatron utilities and tests
    
    * Add encoding tools to tevatron_utils
    
    * Remove unnecessary import from __all__
    
    * Remove another unnecessary import from __all__
    
    * Add retrieve_objects functionality to tevatron utils
    
    * Add tevatron and faiss to pyproject.toml
    
    * Fix fatal error in pyproject.toml
    
    * Remove unnecessary encode_search_corpus function
    
    * Fix docstring describing return type of load_tevatron_model
    
    * Remove unnecessary arg parser
    
    * Update prompt2model/utils/tevatron_utils/encode.py
    
    Co-authored-by: Eren Chenyang Zhao <74843776+zhaochenyang20@users.noreply.github.com>
    
    * Fix indentation from Chenyang's suggestion
    
    * Use with statement instead of try finally
    
    * Update tests/tevatron_utils_test.py
    
    Co-authored-by: Eren Chenyang Zhao <74843776+zhaochenyang20@users.noreply.github.com>
    
    * Collapse multiple equality checks into a single check
    
    * Lint
    
    * Move retrieve.py into this branch instead of the parent branch
    
    ---------
    
    Co-authored-by: Eren Chenyang Zhao <74843776+zhaochenyang20@users.noreply.github.com>

--- prompt2model/utils/__init__.py
+++ prompt2model/utils/__init__.py
@@ -5,12 +5,13 @@ from prompt2model.utils.openai_tools import (
     handle_openai_error,
 )
 from prompt2model.utils.rng import seed_generator
-from prompt2model.utils.tevatron_utils import encode_text
+from prompt2model.utils.tevatron_utils import encode_text, retrieve_objects
 
 __all__ = (  # noqa: F401
     "ChatGPTAgent",
     "encode_text",
     "handle_openai_error",
     "OPENAI_ERRORS",
+    "retrieve_objects",
     "seed_generator",
 )
--- tests/tevatron_utils_test.py
+++ tests/tevatron_utils_test.py
@@ -5,11 +5,12 @@ import os
 import pickle
 import tempfile
 
+import numpy as np
 import pytest
 from tevatron.modeling import DenseModelForInference
 from transformers import PreTrainedTokenizerBase
 
-from prompt2model.utils.tevatron_utils import encode_text
+from prompt2model.utils.tevatron_utils import encode_text, retrieve_objects
 from prompt2model.utils.tevatron_utils.encode import load_tevatron_model
 
 TINY_MODEL_NAME = "google/bert_uncased_L-2_H-128_A-2"
@@ -76,3 +77,37 @@ def test_encode_text_error_from_both_string_and_file():
     file = "/tmp/test.txt"
     with pytest.raises(ValueError):
         _ = encode_text(TINY_MODEL_NAME, file_to_encode=file, text_to_encode=text)
+
+
+def test_retrieve_objects():
+    """Test retrieval against a list of vectors."""
+    mock_query_vector = np.array([[0.0, 0.0, 1.0, 0.0]])
+    # The query vector matches the third row in the search collection.
+    mock_search_collection = np.array(
+        [
+            [1.0, 0.0, 0.0, 0.0],
+            [0.0, 1.0, 0.0, 0.0],
+            [0.0, 0.0, 1.0, 0.0],
+            [0.0, 0.0, 0.0, 1.0],
+        ]
+    )
+    mock_vector_indices = [0, 1, 2, 3]
+    with tempfile.TemporaryDirectory() as tmpdir:
+        search_index_pickle = os.path.join(tmpdir, "search_index.pkl")
+        pickle.dump(
+            (mock_search_collection, mock_vector_indices),
+            open(search_index_pickle, "wb"),
+        )
+        results = retrieve_objects(mock_query_vector, search_index_pickle, depth=3)
+        assert (
+            len(results) == 3
+        ), "The number of results should match the provided depth."
+
+        # Verify that the index of the first retrieved document matches the document
+        # that we known matches the query vector.
+        first_retrieved_document, _ = results[0]
+        assert first_retrieved_document == "2"
+
+        # Verify that the first retrieved document has the greatest retrieval score.
+        sorted_results = sorted(results, key=lambda x: x[1], reverse=True)
+        assert sorted_results[0][0] == first_retrieved_document
