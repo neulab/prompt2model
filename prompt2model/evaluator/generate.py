76556faaa7a7bb632a69bd44cfcbbe1ff4d383c7
zhaochen20 <zhaochenyang20@gmail.com>
2023-06-05 08:42:17 +0800

    detach from document

--- prompt2model/evaluator/generate.py
+++ prompt2model/evaluator/generate.py
@@ -1,77 +1,151 @@
-"""An interface for automatically evaluate Seq2Seq generation model."""
-
-from __future__ import annotations  # noqa FI58
+"""A trainer class to train generation models."""
 
 import logging
 from typing import Any
 
 import datasets
-import evaluate
+import transformers
+from datasets import concatenate_datasets
+from transformers import Trainer, TrainingArguments
 
-from prompt2model.evaluator.base import Evaluator
-from prompt2model.model_executor import ModelOutput
-from prompt2model.prompt_parser import PromptSpec
+from prompt2model.model_trainer.base import BaseTrainer
+from prompt2model.utils import seed_generator
 
 
-class Seq2SeqEvaluator(Evaluator):
-    """An evaluator computing `ChrF++`, `Exact Match` and `Embedding Distance`."""
+class GenerationModelTrainer(BaseTrainer):
+    """Trainer for T5 type (encoder-decoder) model and GPT type (deocder-only) model."""
 
-    def evaluate_model(
+    def __init__(
         self,
-        dataset: datasets.Dataset,
-        gt_column: str,
-        predictions: list[ModelOutput],
-        metrics: list[evaluate.Metric] | None = None,
-        prompt_spec: PromptSpec | None = None,
-        encoder_model_name: str = "xlm-roberta-base",
-    ) -> dict[str, Any]:
-        """Evaluate a model on a test set..
+        pretrained_model_name: str,
+        has_encoder: bool,
+        model_max_length: int | None = None,
+    ):
+        """Initializes a new instance of HuggingFace pre-trained model.
 
         Args:
-            dataset: The dataset to evaluate metrics on.
-            gt_column: The dataset column to use as ground truth.
-            predictions: Model outputs to evaluate.
-            metrics: (Optional) The metrics to use.
-            prompt_spec: (Optional) A PromptSpec to infer the metrics from.
-            encoder_model_name: Encoder model to use in metric like BertScore.
+            pretrained_model_name: HuggingFace pre-trained model name.
+                Only supported encoder-decoder model or atuoregressive model.
+            has_encoder: Whether the model has an encoder.
+                If True, it's a T5-type model (encoder-decoder transformer).
+                If fasle, it's a GPT-type model (atuoregressive transformer).
+            model_max_length: model_max_length allows model to handle
+                longer sequences, and customize sequence lengths as required
+                for your specific use case.
+        """
+        self.has_encoder = has_encoder
+        self.training_args = TrainingArguments(
+            output_dir="./result",
+            logging_steps=1000,
+            evaluation_strategy="steps",
+            eval_steps=1000,
+            save_strategy="steps",
+            save_steps=1000,
+        )
+        if self.has_encoder:
+            self.model = transformers.T5ForConditionalGeneration.from_pretrained(
+                pretrained_model_name
+            )
+            self.tokenizer = transformers.T5Tokenizer.from_pretrained(
+                pretrained_model_name, model_max_length=model_max_length
+            )
+        else:
+            if model_max_length is not None:
+                logging.warning("model_max_length is only supported for T5 models")
+            self.model = transformers.AutoModelForCausalLM.from_pretrained(
+                pretrained_model_name
+            )
+            self.tokenizer = transformers.AutoTokenizer.from_pretrained(
+                pretrained_model_name
+            )
+        if self.tokenizer.pad_token is None:
+            self.tokenizer.add_special_tokens({"pad_token": "[PAD]"})
+        if self.model.config.pad_token_id is None:
+            self.model.config.pad_token_id = len(self.tokenizer)
+            self.model.resize_token_embeddings(len(self.tokenizer))
+            self.model.config.attention_mask_fn = lambda input_ids: (
+                input_ids != self.model.config.pad_token_id
+            ).float()
+
+    def preprocess_dataset(self, dataset: datasets.Dataset):
+        """Preprocesses the given dataset using self.tokenizer.
+
+        Args:
+            dataset: A dictionary-like object containing the input and output columns.
 
         Returns:
-            A dictionary of metric values to return.
+            A `datasets.Dataset` object containing the preprocessed data with:
+                "input_ids": A list of token IDs for the encoded input texts.
+                "attention_mask": A list of 0/1 indicating which tokens are padding.
+                "labels": A list of token IDs for the encoded output texts.
         """
-        if prompt_spec is not None:
-            logging.error("prompt_spec is not supported for Seq2SeqEvaluator")
-            raise NotImplementedError
-        if metrics is not None:
-            logging.error("manual metrics is not supported for Seq2SeqEvaluator")
-            raise NotImplementedError
-        metrics = [
-            evaluate.load("chrf"),
-            evaluate.load("exact_match"),
-            evaluate.load("bertscore"),
-        ]
-        # Get the ground truth from the dataset
-        ground_truth = dataset[gt_column]
+        inputs = dataset["model_input"]
+        outputs = dataset["output_col"]
+        input_encodings = self.tokenizer(inputs, truncation=False, padding=True)
+        output_encodings = self.tokenizer(outputs, truncation=False, padding=True)
 
-        # Extract the predicted strings from ModelOutput
-        predicted_strings = [each.prediction for each in predictions]
+        return datasets.Dataset.from_dict(
+            {
+                "input_ids": input_encodings["input_ids"],
+                "attention_mask": input_encodings["attention_mask"],
+                "labels": output_encodings["input_ids"]
+                if self.has_encoder
+                else input_encodings["input_ids"],
+            }
+        )
 
-        # Initialize the metric values dictionary
-        metric_values = {}
+    def train_model(
+        self,
+        training_datasets: list[datasets.Dataset],
+        hyperparameter_choices: dict[str, Any],
+    ) -> tuple[transformers.PreTrainedModel, transformers.PreTrainedTokenizer]:
+        """Train a text generation model.
 
-        # Compute and store metric values
-        for metric in metrics:
-            metric_name = metric.name
-            assert metric_name in ["chr_f", "exact_match", "bert_score"]
-            if metric_name == "chr_f":
-                metric.add_batch(predictions=predicted_strings, references=ground_truth)
-                metric_values["chr_f++"] = metric.compute(word_order=2)["score"]
-            elif metric_name == "exact_match":
-                metric.add_batch(predictions=predicted_strings, references=ground_truth)
-                metric_values[metric_name] = metric.compute()["exact_match"]
-            elif metric_name == "bert_score":
-                metric.add_batch(predictions=predicted_strings, references=ground_truth)
-                metric_values[metric_name] = metric.compute(
-                    model_type=encoder_model_name
-                )["f1"]
+        Args:
+            training_datasets: Training datasets with `input_col` and `output_col`.
+            hyperparameter_choices: A dictionary of hyperparameter choices.
 
-        return metric_values
+        Returns:
+            A trained HuggingFace model.
+        """
+        self.training_args.output_dir = hyperparameter_choices.get(
+            "output_dir", "./result"
+        )
+        self.training_args.num_train_epochs = hyperparameter_choices.get(
+            "num_train_epochs", 10
+        )
+        self.training_args.per_device_train_batch_size = hyperparameter_choices.get(
+            "batch_size", 100
+        )
+        self.training_args.warmup_steps = hyperparameter_choices.get("warmup_steps", 0)
+        self.training_args.weight_decay = hyperparameter_choices.get(
+            "weight_decay", 0.01
+        )
+        self.training_args.logging_dir = hyperparameter_choices.get(
+            "logging_dir", "./logs"
+        )
+
+        # Concatenate and preprocess the training datasets
+        training_dataset = concatenate_datasets(training_datasets)
+        shuffled_dataset = training_dataset.shuffle(seed=seed_generator.get_seed())
+        preprocessed_dataset = self.preprocess_dataset(
+            shuffled_dataset
+        ).train_test_split(test_size=0.2, seed=seed_generator.get_seed())
+        # Create the trainer
+        trainer = Trainer(
+            model=self.model,
+            args=self.training_args,
+            train_dataset=preprocessed_dataset["train"],
+            eval_dataset=preprocessed_dataset["test"],
+            data_collator=transformers.DataCollatorForSeq2Seq(tokenizer=self.tokenizer)
+            if self.has_encoder
+            else transformers.DataCollatorForLanguageModeling(
+                tokenizer=self.tokenizer, mlm=False
+            ),
+        )
+
+        # Train the model
+        trainer.train()
+
+        # Return the trained model and tokenizer
+        return self.model, self.tokenizer
