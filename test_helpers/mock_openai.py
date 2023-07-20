4f442c513a092adf0e1a6b2530c15c5dc4b111b5
zhaochen20 <zhaochenyang20@gmail.com>
2023-07-20 17:48:39 -0400

    use zeno in a batch

--- test_helpers/mock_openai.py
+++ test_helpers/mock_openai.py
@@ -1,6 +1,9 @@
 """Tools for mocking OpenAI API responses (for testing purposes)."""
 
 
+from __future__ import annotations  # noqa FI58
+
+
 class MockCompletion:
     """Mock openai completion object."""
 
@@ -24,7 +27,7 @@ class MockCompletion:
         return _string
 
 
-def mock_openai_response(
+def mock_one_openai_response(
     prompt: str,
     temperature: float,
     presence_penalty: float,
@@ -49,3 +52,30 @@ def mock_openai_response(
     _ = prompt, temperature, presence_penalty, frequency_penalty
     mock_completion = MockCompletion(content=content)
     return mock_completion
+
+
+def mock_batch_openai_response(
+    prompts: list[str],
+    temperature: float,
+    content: str,
+    presence_penalty: float = 0,
+    frequency_penalty: float = 0,
+) -> list[MockCompletion]:
+    """Generate a batch of  mock completion objects.
+
+        This function creates a batch of `MockCompletion` object with a `content`
+        attribute set to an LLM completion string.
+
+    Args:
+        prompts: A batch of mocked prompts that won't be used.
+        temperature: A mocked temperature.
+        presence_penalty: A mocked presence penalty.
+        frequency_penalty: A mocked frequency penalty.
+        content: The example string to be returned.
+
+    Returns:
+        A mock completion object simulating an OpenAI ChatCompletion API response.
+    """
+    _ = prompts, temperature, presence_penalty, frequency_penalty
+    mock_completions = [MockCompletion(content=content) for _ in prompts]
+    return mock_completions
--- tests/dataset_generator_test.py
+++ tests/dataset_generator_test.py
@@ -10,18 +10,18 @@ import pytest
 from prompt2model.dataset_generator.base import DatasetSplit
 from prompt2model.dataset_generator.openai_gpt import OpenAIDatasetGenerator
 from prompt2model.prompt_parser import MockPromptSpec, TaskType
-from test_helpers import mock_openai_response
+from test_helpers import mock_batch_openai_response
 
 MOCK_CLASSIFICATION_EXAMPLE = partial(
-    mock_openai_response,
+    mock_batch_openai_response,
     content='{"input": "This is a great movie!", "output": "1"}',
 )
 MOCK_WRONG_KEY_EXAMPLE = partial(
-    mock_openai_response,
+    mock_batch_openai_response,
     content='{"input": "This is a great movie!", "label": "1"}',
 )
 MOCK_INVALID_JSON = partial(
-    mock_openai_response,
+    mock_batch_openai_response,
     content='{"input": "This is a great movie!", "output": "1}',
 )
 
@@ -47,7 +47,7 @@ def check_generate_dataset(dataset_generator: OpenAIDatasetGenerator):
     """
     prompt_spec = MockPromptSpec(TaskType.TEXT_GENERATION)
     split = DatasetSplit.TRAIN
-    num_examples = 5
+    num_examples = 6
     # if num_examples >= max_api_calls, the returned dataset's
     # length will be less or equal than max_api_calls.
     dataset = dataset_generator.generate_dataset_split(prompt_spec, num_examples, split)
@@ -100,10 +100,10 @@ def check_generate_dataset_dict(dataset_generator: OpenAIDatasetGenerator):
 
 
 @patch(
-    "prompt2model.utils.ChatGPTAgent.generate_openai_chat_completion",
+    "prompt2model.utils.ChatGPTAgent.generate_batch_openai_chat_completion",
     side_effect=MOCK_CLASSIFICATION_EXAMPLE,
 )
-def test_encode_text(mocked_generate_example):
+def test_api_call_counter(mocked_generate_example):
     """Test classification dataset generation using the OpenAIDatasetGenerator.
 
     This function first test the unlimited generation. Then test generation
@@ -117,18 +117,26 @@ def test_encode_text(mocked_generate_example):
     unlimited_dataset_generator = OpenAIDatasetGenerator()
     check_generate_dataset_dict(unlimited_dataset_generator)
     check_generate_dataset(unlimited_dataset_generator)
-    assert mocked_generate_example.call_count == 11
+    assert mocked_generate_example.call_count == 5
+    # The batch_size is 5 by default. So check_generate_dataset will call
+    # generate_batch_openai_chat_completion 2 times to generate 6 examples.
+    # And the check_generate_dataset_dict will call
+    # generate_batch_openai_chat_completion 3 times for 3 splits.
+    assert unlimited_dataset_generator.api_call_counter == 6 + 3 + 2 + 1
     limited_dataset_generator = OpenAIDatasetGenerator(max_api_calls=3)
     check_generate_dataset(limited_dataset_generator)
-    assert mocked_generate_example.call_count == 14
-    limited_dataset_generator.api_call_counter = 0
+    assert mocked_generate_example.call_count == 6
+    assert limited_dataset_generator.api_call_counter == 3
     # refresh the api_call_counter of limited_dataset_generator for futher test.
+    limited_dataset_generator.api_call_counter = 0
     check_generate_dataset_dict(limited_dataset_generator)
-    assert mocked_generate_example.call_count == 17
+    # Since the max_api_calls is 3, and the api_call_counter is refreshed,
+    # the generate_batch_openai_chat_completion will be called another time.
+    assert mocked_generate_example.call_count == 7
 
 
 @patch(
-    "prompt2model.utils.ChatGPTAgent.generate_openai_chat_completion",
+    "prompt2model.utils.ChatGPTAgent.generate_batch_openai_chat_completion",
     side_effect=MOCK_WRONG_KEY_EXAMPLE,
 )
 def test_wrong_key_example(mocked_generate_example):
@@ -149,7 +157,7 @@ def test_wrong_key_example(mocked_generate_example):
 
 
 @patch(
-    "prompt2model.utils.ChatGPTAgent.generate_openai_chat_completion",
+    "prompt2model.utils.ChatGPTAgent.generate_batch_openai_chat_completion",
     side_effect=MOCK_INVALID_JSON,
 )
 def test_invalid_json_response(mocked_generate_example):
@@ -170,7 +178,7 @@ def test_invalid_json_response(mocked_generate_example):
 
 
 @patch(
-    "prompt2model.utils.ChatGPTAgent.generate_openai_chat_completion",
+    "prompt2model.utils.ChatGPTAgent.generate_batch_openai_chat_completion",
     side_effect=UNKNOWN_GPT3_EXCEPTION(),
 )
 def test_unexpected_examples_of_GPT(mocked_generate_example):
