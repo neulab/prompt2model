bf5a5d077070a5953a65bce8232cb6a6610cf589
viswavi <vijayv@andrew.cmu.edu>
2023-08-04 14:27:21 -0400

    Add DescriptionDatasetRetriever import to dataset_retriever/__init__.py

--- prompt2model/dataset_retriever/__init__.py
+++ prompt2model/dataset_retriever/__init__.py
@@ -1,5 +1,6 @@
 """Import DatasetRetriever classes."""
 from prompt2model.dataset_retriever.base import DatasetRetriever
 from prompt2model.dataset_retriever.mock import MockRetriever
+from prompt2model.dataset_retriever.hf_dataset_retriever import DescriptionDatasetRetriever
 
-__all__ = ("DatasetRetriever", "MockRetriever")
+__all__ = ("DatasetRetriever", "MockRetriever", "DescriptionDatasetRetriever")
