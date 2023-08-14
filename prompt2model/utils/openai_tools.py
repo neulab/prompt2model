06186b0836cbabb851dde8ac189495311d8fe0a3
viswavi <vijayv@andrew.cmu.edu>
2023-08-14 09:38:07 -0400

    Remove duplicated generate_batch_completion function implementation

--- prompt2model/utils/openai_tools.py
+++ prompt2model/utils/openai_tools.py
@@ -178,24 +178,6 @@ class ChatGPTAgent:
         return responses
 
 
-def generate_batch_completion(self, prompt: str) -> openai.Completion:
-    """Generate a chat completion using OpenAI's gpt-3.5-turbo.
-
-    Args:
-        prompt: A prompt asking for a response.
-
-    Returns:
-        A response object.
-    """
-    response = openai.ChatCompletion.create(
-        model=self.model_name,
-        messages=[
-            {"role": "user", "content": f"{prompt}"},
-        ],
-    )
-    return response
-
-
 def handle_openai_error(e, api_call_counter):
     """Handle OpenAI errors or related errors that the OpenAI API may raise.
 
