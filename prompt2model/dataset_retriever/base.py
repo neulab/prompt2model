17599648570d08df063d4c3819a819f9545744ba
Vijay Viswanathan <vijayv@andrew.cmu.edu>
2023-08-22 17:41:19 -0400

    Modify dataset retriever interface (#240)
    * Move mock retrieval index utilities into separate file
    
    * Add script to generate search index of datasets from HuggingFace
    
    * Add dataset index file produced by the retrieve_dataset_info.py script
    
    * Remove old model_info tarball
    
    * Change the dataset retriever interface to return a DatasetDict
    
    * Auto-download model_info tarball from internet
    
    * Remove API key from code
    
    * Update prompt2model/dataset_retriever/retrieve_dataset_info.py
    
    Co-authored-by: Graham Neubig <neubig@gmail.com>
    
    * Update prompt2model/dataset_retriever/retrieve_dataset_info.py
    
    Co-authored-by: Graham Neubig <neubig@gmail.com>
    
    * Update prompt2model/dataset_retriever/retrieve_dataset_info.py
    
    Co-authored-by: Graham Neubig <neubig@gmail.com>
    
    * Move SUPPORTED_TASKS global variable to top of file for visibility
    
    * Remove dependence on model-evaluator submodule
    
    * Remove evaluation metadata from retrieve_dataset_info script
    
    * Untrack pre-constructed dataset search index from repo
    
    ---------
    
    Co-authored-by: Graham Neubig <neubig@gmail.com>

--- prompt2model/dataset_retriever/base.py
+++ prompt2model/dataset_retriever/base.py
@@ -1,5 +1,7 @@
 """An interface for dataset retrieval."""
 
+from __future__ import annotations  # noqa FI58
+
 from abc import ABC, abstractmethod
 
 import datasets
@@ -14,12 +16,12 @@ class DatasetRetriever(ABC):
     @abstractmethod
     def retrieve_dataset_dict(
         self, prompt_spec: PromptSpec
-    ) -> list[datasets.DatasetDict]:
+    ) -> datasets.DatasetDict | None:
         """Retrieve full dataset splits (e.g. train/dev/test) from a prompt.
 
         Args:
             prompt_spec: A prompt spec (containing a system description).
 
         Returns:
-            A list of retrieved DatasetDict containing train/val/test splits.
+            A retrieved DatasetDict containing train/val/test splits.
         """
