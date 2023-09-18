4613f0a37b62e4a7e625e2882855af8f63364ed2
Ritu Gala <39478730+ritugala@users.noreply.github.com>
2023-09-18 10:47:13 -0400

    refactored-first-draft, not tested

--- prompt2model/prompt_parser/instr_parser.py
+++ prompt2model/prompt_parser/instr_parser.py
@@ -13,14 +13,19 @@ from prompt2model.prompt_parser.instr_parser_prompt import (  # isort: split
     construct_prompt_for_instruction_parsing,
 )
 
-from prompt2model.utils import api_tools, get_formatted_logger
+from prompt2model.utils import api_tools, get_formatted_logger, parse_json_responses
 from prompt2model.utils.api_tools import API_ERRORS, handle_api_error
 
 logger = get_formatted_logger("PromptParser")
 
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
-
+# TODO: what to do about max_api_calls
+# TODO: should parse_from_prompt be separate from json_parsing
+# TODO: how to do the try except things with it abstracted to a separate module--prolly done
+# TODO: What to name the file and class?
+# TODO: temparature and all has been abstracted out, mention that in documentation
+# TODO: one of the parse_from_propmt na
 class PromptBasedInstructionParser(PromptSpec):
     """Parse the prompt to separate instructions from task demonstrations."""
 
@@ -38,40 +43,8 @@ class PromptBasedInstructionParser(PromptSpec):
         self.task_type = task_type
         self._instruction: str | None = None
         self._examples: str | None = None
-        if max_api_calls and max_api_calls <= 0:
-            raise ValueError("max_api_calls must be > 0.")
         self.max_api_calls = max_api_calls
-        self.api_call_counter = 0
-
-    def extract_response(self, response: openai.Completion) -> tuple[str, str] | None:
-        """Parse stuctured fields from the API response.
-
-        Args:
-            response: API response.
-
-        Returns:
-            If the API response is a valid JSON object and contains the required_keys,
-                then returns a tuple consisting of:
-                1) Instruction: The instruction parsed from the API response.
-                2) Demonstrations: (Optional) demonstrations parsed from the
-                API response.
-            Else returns None.
-        """
-        response_text = response.choices[0]["message"]["content"]
-        try:
-            response_json = json.loads(response_text, strict=False)
-        except json.decoder.JSONDecodeError:
-            logger.warning(f"API response was not a valid JSON: {response_text}")
-            return None
-
-        required_keys = ["Instruction", "Demonstrations"]
-        missing_keys = [key for key in required_keys if key not in response_json]
-        if len(missing_keys) != 0:
-            logger.warning(f'API response must contain {", ".join(required_keys)} keys')
-            return None
-        instruction_string = response_json["Instruction"].strip()
-        demonstration_string = response_json["Demonstrations"].strip()
-        return instruction_string, demonstration_string
+        
 
     def parse_from_prompt(self, prompt: str) -> None:
         """Parse prompt into specific fields, stored as class member variables.
@@ -84,31 +57,12 @@ class PromptBasedInstructionParser(PromptSpec):
                     "instruction" and "demonstrations".
         """
         parsing_prompt_for_chatgpt = construct_prompt_for_instruction_parsing(prompt)
+        json_parser = parse_json_responses.JsonParsingFromLLMResponse(self.max_api_calls)
+        required_keys = ["Instruction", "Demonstrations"] 
+        #FIXME: The error handling is already done previously isn't it?
+        extraction = json_parser.get_fields_from_llm(parsing_prompt_for_chatgpt, required_keys)
+        self._instruction = extraction['Instruction']
+        self._examples = extraction['Demonstrations']
+        
 
-        chat_api = api_tools.default_api_agent
-        last_error = None
-        while True:
-            self.api_call_counter += 1
-            try:
-                response: openai.ChatCompletion | Exception = (
-                    chat_api.generate_one_completion(
-                        parsing_prompt_for_chatgpt,
-                        temperature=0.01,
-                        presence_penalty=0,
-                        frequency_penalty=0,
-                    )
-                )
-                extraction = self.extract_response(response)
-                if extraction is not None:
-                    self._instruction, self._examples = extraction
-                    return
-            except API_ERRORS as e:
-                last_error = e
-                handle_api_error(e)
 
-            if self.max_api_calls and self.api_call_counter >= self.max_api_calls:
-                # In case we reach maximum number of API calls, we raise an error.
-                logger.error("Maximum number of API calls reached.")
-                raise RuntimeError(
-                    "Maximum number of API calls reached."
-                ) from last_error
