3d4ecf6d92eb84b77549dde5f62fb8d2ea0fbdeb
saum7800 <gandhisaumya8@gmail.com>
2024-03-09 20:44:14 -0500

    remove test issue regarding peft

--- prompt2model/model_trainer/__init__.py
+++ prompt2model/model_trainer/__init__.py
@@ -2,6 +2,5 @@
 from prompt2model.model_trainer.base import BaseTrainer
 from prompt2model.model_trainer.generate import GenerationModelTrainer
 from prompt2model.model_trainer.mock import MockTrainer
-from prompt2model.model_trainer.qlora_trainer import QLoraTrainer
 
-__all__ = ("MockTrainer", "BaseTrainer", "GenerationModelTrainer", "QLoraTrainer")
+__all__ = ("MockTrainer", "BaseTrainer", "GenerationModelTrainer")
